c12146b03152807385f3884ba2ade62b90c717a9
Marcelo Toller <marcelo.toller@yahoo.com.br>
2023-04-10 20:15:09 -0300

    Aulas básicas de Python

---
+++ Basico/basico1.py
@@ -0,0 +1,16 @@
+
+# hashtag para fazer comentários, mas só para uma linha
+
+"""
+DocString
+para mais linhas podemos usar o DocString, mas não é bem 
+um comentário, mais à frente vamos ver, são 3 aspas duplas para abrir e 3 para fechar  
+"""
+
+'''
+o DocString pode ser com 3 aspas simpes tb
+'''
+
+print(123)
+#comentário
+print(456)
---
+++ Basico/print.py
@@ -0,0 +1,6 @@
+# Print pode ser executado com vários argumentos, seja numero ou string.
+# se eu passar 2 argumentos por exemplo ele vai imprimir com espaço entre eles
+# para colocar um separador diferente de espaço utilizamos o sep:
+# pode-se usar aspas simples ou duplas
+
+print(1, 2, 3,4, sep='-')
