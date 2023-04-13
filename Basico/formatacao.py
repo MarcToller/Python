0946a62cde3bb089399a5b7af52affa6406a143f
Marcelo Toller <marcelo.toller@yahoo.com.br>
2023-04-13 13:28:06 -0300

    Aulas básicas de Python

--- Basico/formatacao.py
+++ Basico/formatacao.py
@@ -17,3 +17,20 @@ print(f'pesa {vPeso} quilos e seu IMC é {vImc:.2f}')
 vMilhar = 1000.45
 print(f'{vMilhar:,.2f}')
 
+# FORMAT
+a = 'A'
+b = 'B'
+c = 1.1;
+format = 'a={} b={} c={:.2f}'
+formato = format.format(a,b,c) # lembra o format do Delphi
+print(formato)
+
+# posso passar os indices nas chaves, assim não precisa estar na ordem, possoa té repetir!:
+format = 'a={0} a={0} a={0} b={1} c={2:.2f}'
+formato = format.format(a,b,c) # lembra o format do Delphi
+print(formato)
+
+#parametro nomeado
+format = 'a={nome0} a={nome0} a={nome0} b={nome1} c={nome2:.2f}'
+formato = format.format(nome0=a, nome1=b, nome2=c) # lembra o format do Delphi
+print(formato)
---
+++ Basico/input.py
@@ -0,0 +1,12 @@
+
+# o input sempre retorna uma string
+nome = input('Qual seu nome? ')
+print(f'O seu nome é {nome}') 
+#print(f'O seu nome é {nome=}') #esse "=" mostra o nome da variável e o valor da variável
+
+numero1 = input('Digite um número: ')
+numero2 = input('Digite outro número: ')
+
+#if type(numero1) == int
+
+print(f'A soma dos números é {numero1 + numero2}') 
