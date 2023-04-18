13611f68f8e41426928e308e895dde26d77d0ea3
Marcelo Toller <marcelo.toller@yahoo.com.br>
2023-04-18 20:43:46 -0300

    Aulas básicas de Python

---
+++ Basico/for_in.py
@@ -0,0 +1,8 @@
+
+nome = 'Marcelo Toller'
+
+for letra in nome:
+    print(letra)
+
+for letra in nome[::-1]:
+    print(letra)    
---
+++ Basico/while_for_else.py
@@ -0,0 +1,19 @@
+
+
+
+contador = 0
+
+while contador < 10:
+    contador += 1
+    #if contador == 6:
+    #    break
+    print(contador) 
+else:
+    print('Fim do laço') # vai executar esse else somente se nenhum break for disparado no while
+        
+
+nome = 'toller'
+for letra in nome:
+    print(letra)        
+else:
+    print('terminou laço') # vai executar esse else somente se nenhum break for disparado no while    
