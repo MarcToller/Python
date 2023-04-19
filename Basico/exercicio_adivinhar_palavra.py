1200b0500d91bef29324011dc850b6a7c8f11d8c
Marcelo Toller <marcelo.toller@yahoo.com.br>
2023-04-19 20:53:15 -0300

    Aulas básicas de Python

---
+++ Basico/exercicio_adivinhar_palavra.py
@@ -0,0 +1,43 @@
+import random
+
+
+ListaDePalavras = ['ovo', 'Carro', 'Casa', 'abacate', 'tomate', 'Computador', 'comida']
+JogarDenovo = False
+
+while True:    
+    tentativas = 0
+    indice = random.randint(1, len(ListaDePalavras)-1)
+    PalavraSorteada = ListaDePalavras[indice] 
+    AcumuladorLetra = ''
+    PalavraSorteada = PalavraSorteada.upper()
+
+    if JogarDenovo:
+        JogarDenovo = input('Deseja jogar outra vez? ')
+        if JogarDenovo.upper() == 'N':
+            break
+
+    while True:
+        tentativas += 1
+        letra = input('Digite uma letra: ').upper()
+
+        if len(letra) > 1:
+            print('Digite apenas uma letra!')
+            continue
+        
+        if letra in PalavraSorteada and letra not in AcumuladorLetra:
+            AcumuladorLetra += letra
+        else:
+            print('Palavra não tem essa letra!')    
+            continue
+
+        PalavraDigitada = ''
+        for letra in PalavraSorteada:
+            if letra in AcumuladorLetra:
+                PalavraDigitada += letra
+            else:
+                PalavraDigitada += '*'                      
+        print(PalavraDigitada)
+        if PalavraDigitada == PalavraSorteada:
+            print(f'PARABÉNS, VOCÊ COMPLETOU A PALAVRA EM {tentativas} TENTATIVAS.')
+            JogarDenovo = True
+            break 
