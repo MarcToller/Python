d25bf53192a1f44560c9cc595f1d8fd6f8ada503
Marcelo Toller <marcelo.toller@yahoo.com.br>
2023-04-20 13:31:16 -0300

    Aulas básicas de Python

--- Basico/listas.py
+++ Basico/listas.py
@@ -1,10 +1,22 @@
-
-
-# listas são mutaveis!
-lista = ['teste', 123, True, 2.5]
-
-print(lista)
-
-lista[3] = 'toller'
-
-print(lista)
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3   4   5
+lista = [10, 20, 30, 40]
+# lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+lista.append(50) # adicionano final
+lista.pop() # exclui o último e aindqa retorna o valor que excluiu
+lista.append(60)
+lista.append(70)
+ultimo_valor = lista.pop(3)
+print(lista, 'Removido,', ultimo_valor)
