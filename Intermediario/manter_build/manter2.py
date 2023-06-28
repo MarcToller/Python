4bf388edfdfcc12a0d92979c276f0a992fb7ff60
Marcelo Toller <marcelo.toller@yahoo.com.br>
2023-06-28 20:47:49 -0300

    Módulo intermediário Python

--- Intermediario/manter_build/manter2.py
+++ Intermediario/manter_build/manter2.py
@@ -12,50 +12,55 @@ import json
 
 CHAVE_SISTEMA = 'sistema'
 CHAVE_STATUS = 'status'
-CHAVE_FALHA = 'falha'
 CHAVE_TEMPO = 'tempo'
-VALOR_SEM_FALHA = 'Sem Falha'
-VALOR_COM_FALHA = 'Apresenta Falha'
+VALOR_SEM_FALHA = 'Build OK'
 
 hora_inicio = datetime.now()
 
 lista_resultados = []
 
+print(os.environ['DELPHI_TERCEIROS']) aqui!
 CAMINHO_PASTA = 'C:\Bin.separado\Delphi\Atalhos\Executaveis\ManterBuild'
+
+def verificar_arquivo_saida(nome_arquivo) -> str:    
+    with open(nome_arquivo, 'r') as arquivo:
+        conteudo = arquivo.read()
+        if 'Build Falied' in conteudo:
+            return 'Falha no Build'
+        else:
+            return 'Falha de timeout'
 
 def executar_tarefa(arquivo):
     dicionario_resultado = {}
     try:        
-        dicionario_resultado[CHAVE_SISTEMA] = arquivo 
-        dicionario_resultado[CHAVE_STATUS] = VALOR_COM_FALHA 
+        dicionario_resultado[CHAVE_SISTEMA] = arquivo
+        dicionario_resultado[CHAVE_STATUS] = VALOR_SEM_FALHA        
 
         caminho_completo = os.path.join(CAMINHO_PASTA, arquivo)          
         
         vinicio = datetime.now()  
-        vCaminhoOut = os.path.splitext(arquivo)[0]+'.txt'
-        vCaminhoOut = os.path.join(CAMINHO_PASTA, vCaminhoOut)          
+        vCaminhoArquivoSaida = os.path.splitext(arquivo)[0]+'.txt'
+        vCaminhoArquivoSaida = os.path.join(CAMINHO_PASTA, 'Falhas', vCaminhoArquivoSaida)          
 
-        with open(vCaminhoOut, 'w') as arquivo_saida:
+        with open(vCaminhoArquivoSaida, 'w') as arquivo_saida:
             subprocess.run([caminho_completo, '/NaoExecutar'], shell=True, cwd=CAMINHO_PASTA, timeout=300, stdout=arquivo_saida)
         
         vfinal = datetime.now()  
         dicionario_resultado[CHAVE_TEMPO] = (vfinal - vinicio).total_seconds()
     except subprocess.CalledProcessError:
-        dicionario_resultado[CHAVE_FALHA] = 'Falha desconhecida' 
+        dicionario_resultado[CHAVE_STATUS] = 'Falha Desconhecida' 
         lista_resultados.append(dicionario_resultado)        
         return
-    except subprocess.TimeoutExpired:       
-        dicionario_resultado[CHAVE_FALHA] = 'Falha no Build ou ultrapassou timeout'
-
+    except subprocess.TimeoutExpired:    
+        dicionario_resultado[CHAVE_STATUS] = verificar_arquivo_saida(vCaminhoArquivoSaida)
         lista_resultados.append(dicionario_resultado)                         
         return
-
-    dicionario_resultado[CHAVE_STATUS] = VALOR_SEM_FALHA
+        
+    os.remove(vCaminhoArquivoSaida)
     
     lista_resultados.append(dicionario_resultado)           
 
 max_threads1 = 2
-max_threads2 = 2
 
 #lista_arquivos_pesados = ['04_TestaWinDP.bat', '03_TestaWinContas.bat']
 #lista_arquivos_leves = ['02_TestaFrameWorkDTC.bat', '03_TestaWinContas.bat', '04_TestaWinDP.bat', '01_TestaDCComparaEstruturas.bat']
@@ -70,13 +75,8 @@ with ThreadPoolExecutor(max_workers=max_threads1) as executor1:
         executor1.submit(tarefa1)  
         if sistemas['sistema_2']:
             tarefa1 = partial(executar_tarefa, sistemas['sistema_2'])
-            executor1.submit(tarefa1)        
-
+            executor1.submit(tarefa1)            
     
-    #for arquivo_leve in lista_arquivos_leves:
-    #    tarefa1 = partial(executar_tarefa, arquivo_leve)
-    #    executor1.submit(tarefa1)        
-
 executor1.shutdown(wait=True)
 #executor2.shutdown(wait=True)       
 
--- Intermediario/ziper.py
+++ Intermediario/ziper.py
@@ -6,20 +6,30 @@
 #     intervalo = min(len(l1), len(l2))
 #     return [(l1[i], l2[i]) for i in range(intervalo)]
 from itertools import zip_longest
+import os
 
 l1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
 l2 = ['BA', 'SP', 'MG', 'RJ']
 #print(list(zip(l1, l2)))
 #print(list(zip_longest(l1, l2, fillvalue='SEM CIDADE')))
 
-a = [{'nome': 'marcelo', 'idade': 40},
-     {'nome': 'ana', 'idade': 30}]
+a = [{'nome': 'aaaa', 'idade': 40},
+     {'nome': 'aaaa', 'idade': 30}]
 
 b = [{'nome': 'paulo', 'idade': 4},
      {'nome': 'maria', 'idade': 3}]
 
-Lista = (list(zip(a, b)))
+c = [{'nome': 'www', 'idade': 9},
+     {'nome': 'yyy', 'idade': 7}]
+
+Lista = (list(zip(a, b, c)))
+
+#for tupla in Lista:
+#    for dicionario in tupla:
+#        print(dicionario['nome'], dicionario['idade'])
 
 for tupla in Lista:
-    for dicionario in tupla:
-        print(dicionario['nome'], dicionario['idade'])
+    for dic in tupla:
+     print(dic)
+    
+
