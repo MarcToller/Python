afa632015e56984e432a3ac4378ef65273241959
Marcelo Toller <marcelo.toller@yahoo.com.br>
2023-06-28 17:03:51 -0300

    Módulo intermediário Python

--- Intermediario/manter_build/manter1.py
+++ Intermediario/manter_build/manter1.py
@@ -2,27 +2,39 @@ from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 import subprocess
 import os
+from datetime import datetime
 
-CAMINHO_PASTA = 'C:\\Bin.dtc\\Delphi\\Atalhos\\Executaveis\\ManterBuild'
+data_inicio = datetime.timestamp
+
+listaOK = []
+CAMINHO_PASTA = 'C:\Bin.separado\Delphi\Atalhos\Executaveis\ManterBuild'
 
 def executar_tarefa(arquivo):
-    try:
-        caminho_completo = os.path.join(CAMINHO_PASTA, arquivo)  
-        retorno = subprocess.Popen(caminho_completo, shell=True, cwd=CAMINHO_PASTA, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
-        #retorno = subprocess.call(arquivo, shell=True, cwd=CAMINHO_PASTA) 
-    except:
+    try:        
+        caminho_completo = os.path.join(CAMINHO_PASTA, arquivo)          
+        #retorno = subprocess.run([caminho_completo, '/NaoExecutar'], shell=True, cwd=CAMINHO_PASTA, timeout=300) 
+        #retorno = subprocess.run([caminho_completo, '/NaoExecutar'], shell=True, cwd=CAMINHO_PASTA, capture_output=True, text=True) 
+        retorno = subprocess.run([caminho_completo, '/NaoExecutar'], shell=True, cwd=CAMINHO_PASTA) 
+        #retorno = subprocess.Popen(caminho_completo, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
+        #retorno = subprocess.call([caminho_completo, 'NaoExecutar'], shell=True, cwd=CAMINHO_PASTA, timeout=300)     
+        #retorno = subprocess.call([caminho_completo, '/NaoExecutar'], shell=False, cwd=CAMINHO_PASTA, timeout=30)         
+    except subprocess.CalledProcessError:
         print(f'erro1 em {arquivo}')
+        return
+    except subprocess.TimeoutExpired:
+        print(f'erro2 em {arquivo}')
         return
 
     if retorno.returncode > 0:
-        print(f'erro2 em {arquivo}')
+        print(f'erro3 em {arquivo}')
         return
-    try:
-        retorno.wait(timeout=20)
-    except subprocess.TimeoutExpired:
-        retorno.kill()  
-        return
-    print(f'passou {arquivo}')  
+    listaOK.append(arquivo)    
+    #try:
+     
+    #except subprocess.TimeoutExpired:
+    #    retorno.kill()  
+    #    return
+    #print(f'passou {arquivo}')  
 
     # Código para executar a tarefa
 
@@ -33,9 +45,10 @@ max_threads1 = 1
 max_threads2 = 2
 
 
-
-lista_arquivos_leves = ['01_TestaDCComparaEstruturas.bat', '02_TestaFrameWorkDTC.bat', '03_TestaWinContas.bat', '04_TestaWinDP.bat' ]
-lista_arquivos_pesados = ['ContabMillenium.bat', 'WinCaixaNew.bat', 'WinLalur.bat']
+lista_arquivos_pesados = ['04_TestaWinDP.bat', '03_TestaWinContas.bat']
+#lista_arquivos_leves = ['01_TestaDCComparaEstruturas.bat', '02_TestaFrameWorkDTC.bat', '03_TestaWinContas.bat', '04_TestaWinDP.bat' ]
+#lista_arquivos_pesados = ['ContabMillenium.bat', 'WinCaixaNew.bat', 'WinLalur.bat']
+lista_arquivos_leves = ['01_TestaDCComparaEstruturas.bat', '02_TestaFrameWorkDTC.bat' ]
 
 # Criação dos ThreadPoolExecutors
 with ThreadPoolExecutor(max_workers=max_threads1) as executor1, \
@@ -47,6 +60,15 @@ with ThreadPoolExecutor(max_workers=max_threads1) as executor1, \
 
     for arquivo_pesado in lista_arquivos_pesados:
         tarefa2 = partial(executar_tarefa, arquivo_pesado)
-        executor1.submit(tarefa2)
+        executor2.submit(tarefa2)
 
-print('acabou')        
+#executor1.shutdown(wait=True)
+#executor2.shutdown(wait=True)       
+
+data_fim = datetime.timestamp
+
+#os.system('cls')
+print('acabou')
+#delta = (data_inicio - data_fim).total_seconds()
+#print('Tempo:', delta)
+print(listaOK)
--- Intermediario/manter_build/manter2.py
+++ Intermediario/manter_build/manter2.py
@@ -1,74 +1,100 @@
-
-
 #####  https://chat.openai.com/c/72b35f1a-7df4-4c8f-a447-2aa27bc3605d
 
 
-import os
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 import subprocess
-import threading
-import time;
+import os
+from datetime import datetime
+from sistemas import lista_sistemas
+import json
 
 
-caminho_pasta = 'C:\Bin.dtc\Delphi\Atalhos\Executaveis\ManterBuild'
+CHAVE_SISTEMA = 'sistema'
+CHAVE_STATUS = 'status'
+CHAVE_FALHA = 'falha'
+CHAVE_TEMPO = 'tempo'
+VALOR_SEM_FALHA = 'Sem Falha'
+VALOR_COM_FALHA = 'Apresenta Falha'
+
+hora_inicio = datetime.now()
+
+listaOK = []
+listaErros = []
+
+lista_resultados = []
+
+CAMINHO_PASTA = 'C:\Bin.separado\Delphi\Atalhos\Executaveis\ManterBuild'
+
+def executar_tarefa(arquivo):
+    dicionario_resultado = {}
+    try:        
+        dicionario_resultado[CHAVE_SISTEMA] = arquivo 
+        dicionario_resultado[CHAVE_STATUS] = VALOR_COM_FALHA 
+
+        caminho_completo = os.path.join(CAMINHO_PASTA, arquivo)          
+        
+        vinicio = datetime.now()  
+        vCaminhoOut = os.path.splitext(arquivo)[0]+'.txt'
+        vCaminhoOut = os.path.join(CAMINHO_PASTA, vCaminhoOut)          
+
+        with open(vCaminhoOut, 'w') as arquivo_saida:
+            subprocess.run([caminho_completo, '/NaoExecutar'], shell=True, cwd=CAMINHO_PASTA, timeout=80, stdout=arquivo_saida)
+        
+        vfinal = datetime.now()  
+        dicionario_resultado[CHAVE_TEMPO] = (vfinal - vinicio).total_seconds()
+    except subprocess.CalledProcessError:
+        dicionario_resultado[CHAVE_FALHA] = 'Falha desconhecida' 
+        lista_resultados.append(dicionario_resultado)        
+        return
+    except subprocess.TimeoutExpired:       
+        dicionario_resultado[CHAVE_FALHA] = 'Falha no Build ou ultrapassou timeout'
+
+        lista_resultados.append(dicionario_resultado)                         
+        return
+
+    dicionario_resultado[CHAVE_STATUS] = VALOR_SEM_FALHA
+    
+    lista_resultados.append(dicionario_resultado)           
+
+max_threads1 = 2
+max_threads2 = 2
 
 
-def teste(retorno: subprocess.Popen):
+#lista_arquivos_pesados = ['04_TestaWinDP.bat', '03_TestaWinContas.bat']
+#lista_arquivos_leves = ['01_TestaDCComparaEstruturas.bat', '02_TestaFrameWorkDTC.bat', '03_TestaWinContas.bat', '04_TestaWinDP.bat' ]
+#lista_arquivos_pesados = ['ContabMillenium.bat', 'WinCaixaNew.bat', 'WinLalur.bat']
+#lista_arquivos_leves = ['01_TestaDCComparaEstruturas.bat', '02_TestaFrameWorkDTC.bat', '04_TestaWinDP.bat', '03_TestaWinContas.bat']
 
-    while True:
-        time.sleep(10)
-        output, _ = retorno.communicate(timeout=30)
-        if 'Pressione qualquer tecla para sair'.upper() in str(output).upper():
-            retorno.terminate()
+# Criação dos ThreadPoolExecutors
+with ThreadPoolExecutor(max_workers=max_threads1) as executor1:   
+
+    for sistemas in lista_sistemas:
+        tarefa1 = partial(executar_tarefa, sistemas['sistema_1'])
+        executor1.submit(tarefa1)  
+        if sistemas['sistema_2']:
+            tarefa1 = partial(executar_tarefa, sistemas['sistema_2'])
+            executor1.submit(tarefa1)        
 
     
-## thread = threading.Thread()
+    #for arquivo_leve in lista_arquivos_leves:
+    #    tarefa1 = partial(executar_tarefa, arquivo_leve)
+    #    executor1.submit(tarefa1)        
 
+executor1.shutdown(wait=True)
+#executor2.shutdown(wait=True)       
 
+hora_fim = datetime.now()
 
-## for nome_arquivo in os.listdir(caminho_pasta):
-##     if nome_arquivo[0] != '_':
-#caminho_completo = os.path.join(caminho_pasta, 'ContabMillenium.bat')
-#print(caminho_completo)
-#retorno = subprocess.Popen('ContabMillenium.bat', shell=True, cwd=caminho_pasta, stdout=subprocess.PIPE, stderr=subprocess.PIPE)        
+os.system('cls')
 
-#retorno = subprocess.Popen(caminho_completo, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)        
+tempo_decorrido = (hora_fim - hora_inicio).total_seconds() / 60
+print(f'Tempo: {tempo_decorrido} minutos')
+for resultado in lista_resultados:    
+    print(resultado)
 
-
-#thread = threading.Thread(target=teste(retorno))
-#thread.start()
-#thread.join()
-            
-        
-
-
-
-# for nome_arquivo in os.listdir(caminho_pasta):
-#     if nome_arquivo[0] != '_':
-#         caminho_completo = os.path.join(caminho_pasta, nome_arquivo)
-#         #print(caminho_completo)
-#         retorno = subprocess.Popen(nome_arquivo, shell=True, cwd=caminho_pasta)            
-#         retorno.wait()
-#         if retorno.returncode > 0:
-#               
-#             print('Erro de compilação em', nome_arquivo)
-
-for nome_arquivo in os.listdir(caminho_pasta):
-    if nome_arquivo == 'ContabMillenium.bat':
-        if nome_arquivo[0] != '_':
-            caminho_completo = os.path.join(caminho_pasta, nome_arquivo)
-            #print(caminho_completo)
-            try:
-                #retorno = subprocess.call(nome_arquivo, shell=True, cwd=caminho_pasta) 
-                retorno = subprocess.Popen(nome_arquivo, shell=True, cwd=caminho_pasta, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
-                retorno.communicate()
-
-                thread = threading.Thread(target=teste(retorno))
-                thread.start()
-                thread.join()
-
-                #retorno.wait()
-            except subprocess.CalledProcessError as e:            
-                print('Erro de compilação em', nome_arquivo, e)
-            
-
-
+arquivo_json = caminho_completo = os.path.join(CAMINHO_PASTA, 'resultado.json')
+with open(arquivo_json, 'w') as arquivo:
+    json.dump(lista_resultados, arquivo, indent=4)
+#print('Sistemas OK', listaOK)
+#print('Falhas: ', listaErros)
