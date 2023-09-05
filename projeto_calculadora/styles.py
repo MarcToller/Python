0a976742ad3fcef31a06e77b1cd565fa26d67711
Marcelo Toller <marcelo.toller@yahoo.com.br>
2023-09-04 22:34:21 -0300

    Projeto calculadora

--- projeto_calculadora/styles.py
+++ projeto_calculadora/styles.py
@@ -2,7 +2,9 @@
 # https://doc.qt.io/qtforpython/tutorials/basictutorial/widgetstyling.html
 # Dark Theme
 # https://pyqtdarktheme.readthedocs.io/en/latest/how_to_use.html
-##import qdarktheme
+
+import qdarktheme ## instalando o tema: pip install pyqtdarktheme
+
 from variables import (DARKER_PRIMARY_COLOR, DARKEST_PRIMARY_COLOR,
                        PRIMARY_COLOR)
 
@@ -22,18 +24,17 @@ qss = f"""
 """
 
 
-def setupTheme():
-    ...
-    # qdarktheme.setup_theme(
-    #     theme='dark',
-    #     corner_shape='rounded',
-    #     custom_colors={
-    #         "[dark]": {
-    #             "primary": f"{PRIMARY_COLOR}",
-    #         },
-    #         "[light]": {
-    #             "primary": f"{PRIMARY_COLOR}",
-    #         },
-    #     },
-    #     additional_qss=qss
-    # )
+def setupTheme():    
+    qdarktheme.setup_theme(
+        theme='dark',
+        corner_shape='rounded',
+        custom_colors={
+            "[dark]": {
+                "primary": f"{PRIMARY_COLOR}",
+            },
+            "[light]": {
+                "primary": f"{PRIMARY_COLOR}",
+            },
+        },
+        additional_qss=qss
+    )
